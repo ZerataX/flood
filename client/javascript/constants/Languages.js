8d90f5f4aa4470d114d444c08e3e7210b6d6bb73
Jona Abdinghoff <zeratax@firemail.cc>
2017-06-11 22:17:45 +0200

    Create Languages.js

--- client/javascript/constants/Languages.js
+++ client/javascript/constants/Languages.js
@@ -14,6 +14,10 @@ const LANGUAGES = {
   nl: {
     defaultMessage: 'Nederlands',
     id: 'locale.language.nl'
+  },
+  de: {
+    defaultMessage: 'German',
+    id: 'locale.language.de'
   }
 };
 
